99e6f5eb6d9158bd03273df0b89992f50969b331
Mher Kazandjian <mherkazandjian@gmail.com>
2025-02-27 00:21:51 +0100

    add the pydata themed sample project

---
+++ pydata_sphinx_theme_project/docs/source/conf.py
@@ -0,0 +1,34 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Foo Project XYZ'
+copyright = '2025, john smith'
+author = 'john smith'
+release = 'diamond peak release'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.todo',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+
+html_static_path = ['_static']
+
+html_css_files = []
+
+html_js_files = []
---
+++ pydata_sphinx_theme_project/docs/source/index.rst
@@ -0,0 +1,25 @@
+.. simple project documentation master file, created by
+   sphinx-quickstart on Wed Feb 26 18:56:04 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+simple project documentation
+============================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+Section 1
+---------
+
+This is a simple section. You can add more sections by creating new `.rst`
+
+Section 2
+---------
+
+This is another simple section. You can add more sections by creating new `.rst`
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
